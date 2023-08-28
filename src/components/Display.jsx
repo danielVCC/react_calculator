21fbe5613d36e2529c39e090790908cfa0522be1
Daniel Cavalcante <dvictor.cintra@gmail.com>
2023-08-28 01:30:31 -0300

    update now operators have the same color on display and keyboard

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import './Display.css'
 
+function wrapSpecialCharacters(value) {
+    const specialCharacters = ["x", "%", "/", "-", "+"];
+    const valueArray = value.split('');
+
+    return valueArray.map((char, index) => (
+        specialCharacters.includes(char) ? <span key={index} className="highlight">{char}</span> : char
+    ));
+}
+
 export default props =>
     <div className="display">
-        <div className="main">{props.value}</div>
+        <div className="main">{wrapSpecialCharacters(props.value)}</div>
         <div className="results">{props.resultValue}</div>
     </div>
